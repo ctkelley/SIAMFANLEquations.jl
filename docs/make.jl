cc61e44c0a8d3190e919c89afad56b006e1aa180
Tim Kelley <ctk@ncsu.edu>
2020-04-16 16:08:53 -0400

    Getting docs to work right

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,18 @@
-using Documenter, ScalarEquations, DocumenterLaTeX, DocumenterTools
+using Documenter, SIAMFANLEquations, DocumenterLaTeX, DocumenterTools
 push!(LOAD_PATH,"../src/")
-makedocs(sitename="ScalarEquations.jl",
+makedocs(sitename="SIAMFANLEquations.jl",
 authors="C. T. Kelley",
 format = Documenter.HTML(
                prettyurls = get(ENV, "CI", nothing) == "true"
            ),
 pages = Any[
      "Home" => "index.md",
-     "Scalar Equations Functions" => Any[
+     "Scalar Equation Solvers" => Any[
        "functions/nsolsc.md",
        "functions/sptc.md",
       ]
 ]
 )
 deploydocs(
-     repo="github.com/ctkelley/ScalarEquations.jl.git"
+     repo="github.com/ctkelley/SIAMFANLEquations.jl.git"
 )
