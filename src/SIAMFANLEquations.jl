b1ca2ab51e116ca3967ca8121da808cd388d77fe
Tim Kelley <ctk@ncsu.edu>
2020-07-23 08:10:27 -0400

    Making armojo.jl general (maybe)

--- src/SIAMFANLEquations.jl
+++ src/SIAMFANLEquations.jl
@@ -10,10 +10,12 @@ export difffp
 export fpeval_newton
 export ptcsolsc
 export printhist
+export armijosc
 
 include("Tools/difffp.jl")
 include("Tools/fpeval_newton.jl")
 include("Tools/parab3p.jl")
+include("Tools/armijo.jl")
 include("Chapter1/nsolsc.jl")
 include("Chapter1/ptcsolsc.jl")
 include("PlotsTables/printhist.jl")
