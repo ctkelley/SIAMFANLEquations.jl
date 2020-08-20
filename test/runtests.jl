78c30111bfd4ebe751c7d410a1032982b450988e
Tim Kelley <ctk@ncsu.edu>
2020-08-20 16:06:51 -0400

    testing bvp

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,16 +2,19 @@ using SIAMFANLEquations
 using SIAMFANLEquations.TestProblems
 using Test
 using LinearAlgebra
+
 include("Chapter1/nsolsc_solution_test.jl")
 include("Chapter1/ptcsolsc_test.jl")
 include("Chapter2/basic2d_test.jl")
 include("Chapter2/heq_lu_test.jl")
+include("Chapter2/bvp_test.jl")
 @testset "Scalar Equations: Chapter 1" begin
    @test nsolsc_solution_test()
    @test ptcsolsc_test()
 end
 @testset "nsold: Chapter 2" begin
     @test basic2d_test()
+    @test bvp_test(201)
 end
 @testset "H-equation: Chapter 2" begin
    @test heq_lu_test()
