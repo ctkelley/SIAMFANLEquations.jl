82d4e930ec7b7e99adb0f3342978efddd3f0cba5
Tim Kelley <ctk@ncsu.edu>
2020-09-17 12:48:00 -0400

    Fixing things

--- src/SIAMFANLEquations.jl
+++ src/SIAMFANLEquations.jl
@@ -75,49 +75,4 @@ include("TestProblems/Systems/Hequation.jl")
 
 end
 
-
-module NotebookSIAMFANL
-
-using PyPlot
-using LaTeXStrings
-using Printf
-using LinearAlgebra
-using SIAMFANLEquations
-using SIAMFANLEquations.TestProblems
-
-export atan_test
-export ftan_test
-export ptc_scalar_example
-export tab1dot1
-export fig1dot1
-export fig1dot2
-export fig1dot3
-export fig1dot3b
-export fig1dot4
-export fig1dot5
-export fig1dot6
-export fig1dot7
-export fig2dot1
-export MyFunction
-export PitchFork1
-
-export plothist
-
-include("Notebook/Chapter1/fig1dot1.jl")
-include("Notebook/Chapter1/fig1dot2.jl")
-include("Notebook/Chapter1/fig1dot3.jl")
-include("Notebook/Chapter1/fig1dot3b.jl")
-include("Notebook/Chapter1/fig1dot4.jl")
-include("Notebook/Chapter1/fig1dot5.jl")
-include("Notebook/Chapter1/fig1dot6.jl")
-include("Notebook/Chapter1/fig1dot7.jl")
-include("Notebook/Chapter1/tab1dot1.jl")
-include("Notebook/Chapter1/MyFunction.jl")
-include("Notebook/Chapter1/PitchFork1.jl")
-include("Notebook/Chapter2/fig2dot1.jl")
-
-include("Notebook/Tools/plothist.jl")
-
-end # module NotebookSIAMFANL
-
 end # module
