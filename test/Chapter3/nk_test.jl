6c4c2b1c00fa492d8597c4dafaa9159fc5bc69d0
Tim Kelley <ctk@ncsu.edu>
2021-01-27 18:27:44 -0500

    test Eisenstat-Walker

--- test/Chapter3/nk_test.jl
+++ test/Chapter3/nk_test.jl
@@ -24,11 +24,11 @@ FS=copy(x0)
 # Newton and forward difference directional derivatives for Newton-GMRES
 #
 dout=nsol(simple!, x0, FS, FPJ, jsimple!; sham=1, keepsolhist=true)
-kout=nsoli(simple!, x0, FS, FPS ;eta=1.e-10, fixedeta=true, 
+kout=nsoli(simple!, x0, FS, FPS ;eta=1.e-10,
                   lmaxit=3, keepsolhist=true)
 dsolhist=norm(kout.solhist-dout.solhist,Inf)
-shpass=(dsolhist < 1.e-7)
-shpass || println("solhist compare fails in nksimple", dsolhist)
+shpass=(dsolhist < 1.e-11)
+shpass || println("solhist compare fails in easy nksimple", dsolhist)
 vconverge = krstest(dout,kout)
 #
 # For the stagnating problem we will do analytic Jacobians for
@@ -38,6 +38,7 @@ x0=[3.0;5.0]
 dout=nsol(simple!, x0, FS, FPJ, jsimple!; sham=1)
 kout=nsoli(simple!, x0, FS, FPS, Jvsimple; fixedeta=true, eta=1.e-10, lmaxit=2)
 vdiverge = krstest(dout,kout)
+vdiverge || println("failure hard nksimple problem")
 return vconverge && vdiverge && shpass
 end
 
