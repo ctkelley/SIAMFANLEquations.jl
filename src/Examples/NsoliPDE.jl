cd02e8a33db948089aeee1088e132b9eb41f8441
C. T. (Tim) Kelley <tim_kelley@ncsu.edu>
2021-02-09 10:11:36 -0500

    Last update before taggin 0.3.0

--- src/Examples/NsoliPDE.jl
+++ src/Examples/NsoliPDE.jl
@@ -4,16 +4,23 @@ NsoliPDE(n; fixedeta=true, eta=.1)
 Solve the Elliptic PDE using nsol.jl on an n x n grid. You give me
 n and (optionally) sham and resdec and I return the output of nsol.
 """
-function NsoliPDE(n; eta=.1, fixedeta=true, 
-               rtol=1.e-7, atol=1.e-10, Pvec=Pvec2d, pside="right")
-# Get some room for the residual
+function NsoliPDE(
+    n;
+    eta = 0.1,
+    fixedeta = true,
+    rtol = 1.e-7,
+    atol = 1.e-10,
+    Pvec = Pvec2d,
+    pside = "right",
+)
+    # Get some room for the residual
     u0 = zeros(n * n)
     FV = copy(u0)
     # Get the precomputed data from pdeinit
     pdata = pdeinit(n)
     # Storage for the Krylov basis
     JV = zeros(n * n, 100)
-pout = nsoli(
+    pout = nsoli(
         pdeF!,
         u0,
         FV,
@@ -28,5 +35,5 @@ pout = nsoli(
         maxit = 20,
         pside = pside,
     )
-return pout
+    return pout
 end
