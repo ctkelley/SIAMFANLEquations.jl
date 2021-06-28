1d59ac5abbc5042471ace6e73c56c4f3f44832fc
C. T. (Tim) Kelley <tim_kelley@ncsu.edu>
2021-06-28 12:15:24 -0400

    Bug fix

--- src/TestProblems/Systems/simple!.jl
+++ src/TestProblems/Systems/simple!.jl
@@ -6,6 +6,7 @@ It also shows up in CI
 function simple!(FV, x)
     FV[1] = x[1] * x[1] + x[2] * x[2] - 2.0
     FV[2] = exp(x[1] - 1) + x[2] * x[2] - 2.0
+    return FV
 end
 
 function jsimple!(JacV, FV, x)
@@ -13,6 +14,7 @@ function jsimple!(JacV, FV, x)
     JacV[1, 2] = 2.0 * x[2]
     JacV[2, 1] = exp(x[1] - 1)
     JacV[2, 2] = 2 * x[2]
+    return JacV
 end
 
 """
