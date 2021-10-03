337f894e7b8eaf5704882433b9b6ba5fb59699b6
Tim Kelley <ctk@ncsu.edu>
2021-10-03 14:43:01 -0400

    Found it, now making the adjustment

--- test/Chapter4/ci_pde_aa.jl
+++ test/Chapter4/ci_pde_aa.jl
@@ -11,14 +11,13 @@ VstoreS=zeros(n*n,2*m+4);
 aout=PDE_aa(n, m; Vstore=Vstore, pdata=pdata);
 aoutS=PDE_aa(n, m; Vstore=VstoreS, pdata=pdata);
 # Same results with low storage mode?
-pdeerrS= (
-        norm(aoutS.history-aout.history,Inf) +
-norm(aoutS.stats.condhist-aout.stats.condhist,Inf) +
-        norm(aoutS.stats.alphanorm-aout.stats.alphanorm,Inf)
-    )
+condS = norm(aoutS.stats.condhist-aout.stats.condhist,Inf)
+alphaS = norm(aoutS.stats.condhist-aout.stats.condhist,Inf)/norm(aout.stats.condhist,Inf)
+histS = norm(aoutS.history-aout.history,Inf)
+pdeerrS=  condS + alphaS + histS
 aout.idid || println("pde solver failed")
 (aout.errcode == 0) || println("wrong error code in pde")
-(pdeerrS < 1.e-15) || println("different stats   ",pdeerrS)
+(pdeerrS < 1.e-15) || println("different stats   ",condS,"  ",alphaS,"  ",histS)
 (length(aout.history)==21) || println("history length wrong")
 aa_ok = aout.idid && (aout.errcode==0) && (length(aout.history)==21) && (pdeerrS < 1.e-12)
 aa_ok && println("pde succeeds")
